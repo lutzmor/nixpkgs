3ffcfffa5a9f2176829ef649e906106383017cb3
Moritz Lutz <moritz.lutz@gmail.com>
2022-01-07 20:02:57 +0100

    Update default.nix

--- pkgs/os-specific/darwin/apple-sdk-11.0/default.nix
+++ pkgs/os-specific/darwin/apple-sdk-11.0/default.nix
@@ -8,7 +8,7 @@ let
 
     # https://swscan.apple.com/content/catalogs/others/index-11-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog
     src = fetchurl {
-      url = "http://swcdn.apple.com/content/downloads/46/21/001-89745-A_56FM390IW5/v1um2qppgfdnam2e9cdqcqu2r6k8aa3lis/CLTools_macOSNMOS_SDK.pkg";
+      url = "https://swcdn.apple.com/content/downloads/50/51/071-29699-A_YC8SX0OHH3/7479xojqghsvgtnt3dxjpnxuz9sjpmbmds/CLTools_macOSNMOS_SDK.pkg";
       sha256 = "0n425smj4q1vxbza8fzwnk323fyzbbq866q32w288c44hl5yhwsf";
     };
 
